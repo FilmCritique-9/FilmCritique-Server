7b3fcc993b3ea5a15c77c7d75db9138f981faf8d
handa <handa0704@naver.com>
2024-06-07 20:11:44 +0900

    url 추가

--- config/settings.py
+++ config/settings.py
@@ -28,11 +28,13 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'http://localhost:3000',
-    #'http://127.0.0.1:8000',
-    'port-0-filmcritique-server-1272llwumfcoj.sel5.cloudtype.app/'
-]
+# ALLOWED_HOSTS = [
+#     'http://localhost:3000',
+#     #'http://127.0.0.1:8000',
+#     'port-0-filmcritique-server-1272llwumfcoj.sel5.cloudtype.app/'
+# ]
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -145,11 +147,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # cors 설정
 CORS_ALLOWED_CREDENTIALS = True
-# CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    #'http://127.0.0.1:8000',
-    'https://port-0-filmcritique-server-1272llwumfcoj.sel5.cloudtype.app/'
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     #'http://127.0.0.1:8000',
+#     'https://port-0-filmcritique-server-1272llwumfcoj.sel5.cloudtype.app/'
+# ]
 
 # csrf 설정
 CSRF_TRUSTED_ORIGINS = [
