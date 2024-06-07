366fb7b450b79424d51b1d463f546fa201bd5f61
handa <handa0704@naver.com>
2024-06-07 20:20:01 +0900

    url / 제거

--- config/settings.py
+++ config/settings.py
@@ -147,15 +147,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # cors 설정
 CORS_ALLOWED_CREDENTIALS = True
-#CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    #'http://127.0.0.1:8000',
-    'https://port-0-filmcritique-server-1272llwumfcoj.sel5.cloudtype.app'
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     #'http://127.0.0.1:8000',
+#     'https://port-0-filmcritique-server-1272llwumfcoj.sel5.cloudtype.app'
+# ]
 
 # csrf 설정
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
-    #'http://127.0.0.1:8000',
+    'http://127.0.0.1:8000',
     'https://port-0-filmcritique-server-1272llwumfcoj.sel5.cloudtype.app'
 ]
